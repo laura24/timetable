ac5ddc08c773548049eebde2293f0a419a21d86b
Cristina Opriceană <cristina@devel.ccna.ro>
2012-09-17 11:31:35 +0300

    after the dawn

--- index.php
+++ index.php
@@ -5,29 +5,36 @@ global $MODIFIER,$DB,$OUTPUT;
 
   require_once('../../config.php');
   require_once('lib.php');
-<<<<<<< HEAD
+
   
-	$current_year = date('Y', time()+3600*356*24 ); 
-	echo $current_year;
-	define("TIME_TO_DELETE", $current_year."-02-15");
-	$aux = strtotime(TIME_TO_DELETE );
-	
-	if( time()> $aux ) 
-	{
-		$rec = $DB->get_records('timetable');
-		foreach ($rec as $r) 
-		{
-			timetable_delete_instance($r->id);
-		}
-		
-	}
-	 echo timetable_display();
-=======
-  $rec = $DB->get_records('timetable');
-  foreach($rec as $r) 
-  {
-	$aux = $r->timecreated + 360*24*250;
-  }
+$current_year = date('Y', time()); 
+    echo $current_year;
+   
+    define("AUTUMN_SESSION", $current_year."-09-17");
+    define("SPRING_SESSION", $current_year."-06-15");
+    define("SUMMER_SESSION", $current_year."-08-15");
+    $aux1 = strtotime(AUTUMN_SESSION );
+    $aux2 = strtotime(SPRING_SESSION );
+
+    $aux3 = strtotime(SUMMER_SESSION );
+
+
+    if( (time() > $aux1 && time() < ($aux1+24*3600)) 
+        || (time() > $aux2 && time() < ($aux2+24*3600)) 
+            || (time() > $aux3 && time() < ($aux3+24*3600)) )
+    {
+
+            print "we are here";
+            $rec = $DB->get_records('timetable_base');
+            foreach ($rec as $r) 
+            {
+            timetable_update_active($r);
+            }
+
+
+    }
+    
+
+
   echo timetable_display();
->>>>>>> 463c76c207cc88c95ce8a9c890ad84e7369679c6
-  
+
--- lib.php
+++ lib.php
@@ -123,12 +123,31 @@ function timetable_cron() {
      * Updates the cached HTML timetable
      * @return  nothing
      */
-    global $TIMETABLE_HTML_FILE;
-    $filehandler = fopen($TIMETABLE_HTML_FILE, 'w');
-    
-    fwrite($filehandler, timetable_display());
-    
-    fclose($filehandler);
+    $current_year = date('Y', time()+3600*356*24 ); 
+    echo $current_year;
+    define("AUTUMN_SESSION", $current_year."-09-17");
+    define("SPRING_SESSION", $current_year."-06-15");
+    define("SUMMER_SESSION", $current_year."-08-15");
+    $aux1 = strtotime(AUTUMN_SESSION );
+    $aux2 = strtotime(SPRING_SESSION );
+
+    $aux3 = strtotime(SUMMER_SESSION );
+
+
+    /*if( (time() > $aux1 && time() < ($aux1+24*3600)) 
+        || (time() > $aux2 && time() < ($aux2+24*3600)) 
+            || (time() > $aux3 && time() < ($aux3+24*3600)) )
+    {
+
+            print "we are here";
+            $rec = $DB->get_records('timetable_base');
+            foreach ($rec as $r) 
+            {
+            timetable_update_active($r);
+            }
+
+
+    }*/
     
 }
 
@@ -138,5 +157,22 @@ function timetable_build_course_url($course) {
     return $CFG->wwwroot.'/course/view.php?id='.$course->id;
 }
 
+
+
+
+function timetable_update_active($record){
+
+    global $DB;
+
+    $record->active=1;
+    $record_id = $record->id;
+
+    $DB->delete_records('timetable_base', array('id'=>$record_id));
+
+    $DB->insert_record('timetable_base', $record);
+
+    return (integer)$record_id;
+
+}
 
 ?>
